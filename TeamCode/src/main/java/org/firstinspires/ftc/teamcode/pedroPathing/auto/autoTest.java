5009a292683851add054e9754fd1791937214db3
EventHorizon31212 <ftceventhorizon@gmail.com>
2025-08-30 19:11:42 -0500

    new repo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/auto/autoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/auto/autoTest.java
@@ -6,7 +6,6 @@ import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 @Autonomous(name = "autoTest", group = "Robot")
